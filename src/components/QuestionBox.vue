1db2e230fce9c4197b666c7c20313350bc40f2eb
Coaxial <thecoaxial@gmail.com>
2021-06-19 13:21:17 -0400

    Stlyed the question box and made it display data from the Open Trivia db API

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -3,17 +3,63 @@
     <b-jumbotron >
 
     <template #lead>
-      Some Question Here
+      {{ currentQuestion.question }}
     </template>
 
     <hr class="my-4">
 
-    <p>
-      List of answers here
-    </p>
+    <b-list-group>
+    <b-list-group-item
+        v-for="(answer, index) in answers" 
+        :key="index"
+        @click="selectAnswer(index)"
+    >
+    {{ answer }}
+    </b-list-group-item>
+    </b-list-group>
 
     <b-button variant="primary" href="#">Submit</b-button>
-    <b-button variant="success" href="#">Next Question</b-button>
+    <b-button @click="next" variant="success" href="#">
+        Next Question</b-button>
     </b-jumbotron>
 </div>
 </template>
+
+<script>
+export default {
+    props: {
+        currentQuestion: Object,
+        next: Function
+    },
+    data() {
+        return {
+            selectedIndex: null
+        }
+    },
+    computed: {
+        answers() {
+            let answers = [...this.currentQuestion.incorrect_answers]
+            answers.push(this.currentQuestion.correct_answer)
+            return answers
+        }
+    },
+    methods: {
+        selectAnswer(index) {
+            this.selectedIndex = index
+        }
+    },
+    mounted() {
+        console.log(this.currentQuestion)
+    }
+}
+</script>
+
+<style scoped>
+.list-group {
+    margin-bottom: 15px;
+}
+.btn {
+    margin: 0 5px;
+}
+</style>
+
